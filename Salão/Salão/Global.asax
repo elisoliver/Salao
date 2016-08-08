5aee445482ce795e9531901a05941c2e0299dfc7
elis-oliveira <elis.oliveira@fatec.sp.gov.br>
2016-08-08 07:52:24 -0300

    First Commit

---
+++ Salão/Salão/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Salão.MvcApplication" Language="C#" %>
---
+++ Salão/Salão/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Salão.Startup))]
+namespace Salão
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
