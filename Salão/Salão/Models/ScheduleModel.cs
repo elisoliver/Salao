5537000601edc001417c5eeba60e433f91c27a96
elisoliver <elis_oliver@live.com>
2016-08-10 11:28:34 -0300

    CRUD Employee

---
+++ Salão/Salão/Models/ScheduleModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Salão.Models
+{
+    public class ScheduleModel
+    {
+        public int ID { get; set; }
+
+        [Display(Name = "Data")]
+        [DataType(DataType.DateTime)]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd/mm/yyyy}")]
+        public DateTime DateSchedule { get; set; }
+        public AdminModel User { get; set; }
+    }
+}
